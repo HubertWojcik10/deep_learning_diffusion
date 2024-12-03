dcb3e1e0ded4bde4bf78ffcf2c99f83be188bc6b
German <s243660@dtu.dk>
2024-12-03 17:19:36 +0100

    all clean

--- sample_ddpm.py
+++ sample_ddpm.py
@@ -3,27 +3,23 @@ import torchvision
 import os
 from torchvision.utils import make_grid
 from tqdm import tqdm
+from model.unet import Unet
+from noise_scheduler._archived_scheduler import NoiseScheduler
+from utils.load_params import load_params
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def sample(model, scheduler, config):
-    """
+    r"""
     Sample stepwise by going backward one timestep at a time.
     We save the x0 predictions
     """
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    xt = torch.randn((config['batch_size'],
+    xt = torch.randn((config['num_samples'],
                       config['channels_num'],
                       config['image_size'],
                       config['image_size'])).to(device)
-
-    # Create output directories using relative paths
-    task_dir = os.path.join('.', config['task_name'])  # Start from current directory
-    samples_dir = os.path.join(task_dir, 'samples')
-    
-    # Create directories if they don't exist
-    os.makedirs(task_dir, exist_ok=True)
-    os.makedirs(samples_dir, exist_ok=True)
-
     for i in tqdm(reversed(range(config['num_timesteps']))):
         # Get prediction of noise
         noise_pred = model(xt, torch.as_tensor(i).unsqueeze(0).to(device))
@@ -31,17 +27,17 @@ def sample(model, scheduler, config):
         # Use scheduler to get x0 and xt-1
         xt, x0_pred = scheduler.sample_prev_timestep(xt, noise_pred, torch.as_tensor(i).to(device))
         
-        if i % 100 == 0:
-            # Save x0
-            ims = torch.clamp(xt, -1., 1.).detach().cpu()
-            ims = (ims + 1) / 2
-            grid = make_grid(ims, nrow=config['num_grid_rows'])
-            img = torchvision.transforms.ToPILImage()(grid)
-        
-            # Save the image
-            img.save(os.path.join(samples_dir, f'x0_{i}.png'))
-            img.close()
-            
+        # Save x0
+        ims = torch.clamp(xt, -1., 1.).detach().cpu()
+        ims = (ims + 1) / 2
+        grid = make_grid(ims, nrow=config['num_grid_rows'])
+        img = torchvision.transforms.ToPILImage()(grid)
+        if not os.path.exists(os.path.join(config['task_name'], 'samples')):
+            os.mkdir(os.path.join(config['task_name'], 'samples'))
+        img.save(os.path.join(config['task_name'], 'samples', 'x0_{}.png'.format(i)))
+        img.close()
+
+
 def infer():
     config = load_params("config.json")
     
